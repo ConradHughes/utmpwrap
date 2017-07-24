d8520dcf543c9f096a74e41d405954d8a2018187
Conrad Hughes <ConradHughes@users.noreply.github.com>
2017-07-24 22:06:42 +0100

    Better error reporting.

--- utmp_wrap.c
+++ utmp_wrap.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -9,19 +10,30 @@ main(int argc, char **argv)
 {
   pid_t child;
 
-  if(utempter_add_record(STDIN_FILENO, (char *) NULL) != 0)
+  if(utempter_add_record(STDIN_FILENO, (char *) NULL) == 0) {
+    perror("Couldn't add record to utmp");
     return EXIT_FAILURE;
-
-  if((child = fork())) {
-    if(child < 0)
-      return EXIT_FAILURE;
-    if(waitpid(child, (int *) NULL, 0) != child)
-      return EXIT_FAILURE;
-    if(utempter_remove_record(STDIN_FILENO) != 0)
-      return EXIT_FAILURE;
-    return EXIT_SUCCESS;
   }
 
-  execv(argv[1], argv + 2);
-  /*NOTREACHED*/
+  if((child = fork()) == 0) {
+    execv(argv[1], argv + 2);
+    /*NOTREACHED*/
+  }
+
+  if(child < 0) {
+    perror("fork failed");
+    return EXIT_FAILURE;
+  }
+
+  if(waitpid(child, (int *) NULL, 0) != child) {
+    perror("waitpid failed");
+    return EXIT_FAILURE;
+  }
+
+  if(utempter_remove_record(STDIN_FILENO) == 0) {
+    perror("Couldn't remove record from utmp");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
